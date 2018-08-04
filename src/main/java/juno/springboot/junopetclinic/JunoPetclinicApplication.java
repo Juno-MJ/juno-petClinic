a534f055ef7bfaaea5e1cb4514f059048eaf03d4
Prakash <prakas.kanna.subramanian@gmail.com>
2018-08-04 21:20:55 +0530

    Initial Update to Git

---
+++ src/main/java/juno/springboot/junopetclinic/JunoPetclinicApplication.java
@@ -0,0 +1,12 @@
+package juno.springboot.junopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunoPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunoPetclinicApplication.class, args);
+    }
+}
