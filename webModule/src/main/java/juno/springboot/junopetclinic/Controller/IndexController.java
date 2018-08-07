bb880c0cbe120c10ddf605832d673de7e0977cc0
Prakash <prakas.kanna.subramanian@gmail.com>
2018-08-08 00:56:56 +0530

    closes #8

---
+++ webModule/src/main/java/juno/springboot/junopetclinic/Controller/IndexController.java
@@ -0,0 +1,14 @@
+package juno.springboot.junopetclinic.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String indexRedirect(){
+        return "index";
+    }
+
+}
