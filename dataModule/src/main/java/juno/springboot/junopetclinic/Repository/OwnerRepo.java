57551b90ab841746613d493c730743fce0b834ac
Prakash <prakas.kanna.subramanian@gmail.com>
2018-08-19 02:11:22 +0530

    closes #40

--- dataModule/src/main/java/juno/springboot/junopetclinic/Repository/OwnerRepo.java
+++ dataModule/src/main/java/juno/springboot/junopetclinic/Repository/OwnerRepo.java
@@ -1,9 +1,17 @@
 package juno.springboot.junopetclinic.Repository;
 
 import juno.springboot.junopetclinic.Model.Owner;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository("ownerRepo")
 public interface OwnerRepo extends CrudRepository<Owner,Long> {
 
+    @Query("SELECT o FROM Owner o where o.lastName like %:lastname%")
+    Optional<Owner> findByLastName(@Param("lastname") String lastName);
 
 }
---
+++ dataModule/src/main/java/juno/springboot/junopetclinic/Services/springDataJPA/ownerSDJpa.java
@@ -0,0 +1,54 @@
+package juno.springboot.junopetclinic.Services.springDataJPA;
+
+import juno.springboot.junopetclinic.Model.Owner;
+import juno.springboot.junopetclinic.Repository.OwnerRepo;
+import juno.springboot.junopetclinic.Services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("SDJPA")
+public class ownerSDJpa implements OwnerService {
+
+    private final OwnerRepo ownerRepo;
+
+    public ownerSDJpa(OwnerRepo ownerRepo) {
+        this.ownerRepo = ownerRepo;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepo.findByLastName(lastName).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        ownerRepo.save(object);
+        return object;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> ownerSet = new HashSet<>();
+        ownerRepo.findAll().forEach(ownerSet::add);
+        return ownerSet;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+       return ownerRepo.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepo.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepo.deleteById(aLong);
+    }
+}
